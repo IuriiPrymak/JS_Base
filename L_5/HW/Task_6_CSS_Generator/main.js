818cf5a86e7dedc1377a5fc90c86994062f41676
IuriiPrymak <yura272@gmail.com>
2020-12-21 17:59:56 +0200

    commit 21.12.2020

---
+++ L_5/HW/Task_6_CSS_Generator/main.js
@@ -0,0 +1,25 @@
+var rangeTl = document.getElementById('tlr'),
+    rangeTr = document.getElementById('trr'),
+    rangeBl = document.getElementById('blr'),
+    rangeBr = document.getElementById('brr');
+
+var resultTl = document.getElementById('result-tlr'),
+    resultTr = document.getElementById('result-trr'),
+    resultBl = document.getElementById('result-blr'),
+    resultBr = document.getElementById('result-brr');
+
+var input = document.querySelectorAll('.input');
+var cube = document.getElementById('cube');
+
+var changeRadius = () => {
+    resultTl.innerHTML = rangeTl.value;
+    resultTr.innerHTML = rangeTr.value;    
+    resultBl.innerHTML = rangeBl.value;
+    resultBr.innerHTML = rangeBr.value;
+
+    cube.style.margin = rangeTl.value + 'px ' + rangeTr.value + 'px ' + rangeBr.value + 'px ' + rangeBl.value + 'px';
+}
+
+for(node of input) {
+    node.addEventListener('input', changeRadius);
+}
